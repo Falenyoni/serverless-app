4321937c2f507470daa4938663cba11d7c50d4df
Bongani Nyoni <b.nyoni1101@gmail.com>
2023-12-16 12:42:19 +0200

    first commit Contact Page

---
+++ README.md
@@ -0,0 +1,23 @@
+## Contact Form Project
+
+# This project Is used to demonstrate a Serverless 3-Tier Application in AWS:
+# The AWS Services used are:
+    1. AWS API Gateway
+    2. Lambda Function
+    3. DynamoDB
+    4. S3 For  (React Front End)
+
+With this Project You can Practice using Varius AWS Services as well. Below are some of the varied USE.
+
+        1. Host React App in an EC2 instance, the backend can be a SQL database(MYSQL, Postgres, SQL SERVER, Aurora) within the Same instance - You can manipulate the code however you like
+        2. Host React App in an EC2 instance, Backend can Be a Dynamo DB
+        3. Cloud Front Distribution for you S3 Hosted App and any Backend
+        4. Create Step Functions to Send Email and Save to DB
+
+
+## Things To improbe
+1. Create a redirect page After Saving Data,
+2. Create CI/CD for Froject
+3. Create CDK Project
+4. Create A Terraform Project
+5. Pass Parameters to Lambda Function.
---
+++ src/ContactPage.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onNameChanged = (e) => setName(e.target.value);
+  const onEmailChanged = (e) => setEmail(e.target.value);
+  const onPhoneChanged = (e) => setPhone(e.target.value);
+  const onMessageChanged = (e) => setMessage(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+    console.log(message);
+
+    
+
+    setName("");
+    setEmail("");
+    setPhone("");
+    setMessage("");
+
+    //console.log(response.data);
+  };
+
+  return (
+    <div className="font-[sans-serif] max-w-6xl mx-auto">
+      <div className="bg-white shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] my-6 p-8 rounded">
+        <h2 className="text-2xl text-[#333] font-extrabold text-center mb-12">
+          Contact Us
+        </h2>
+        <form className="max-w-xl mx-auto" onSubmit={handleSubmit}>
+          <div className="space-y-4">
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-full border rounded py-3 px-4 text-sm focus:outline-blue-600"
+              id="name"
+              name="name"
+              onChange={onNameChanged}
+              value={name}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full border rounded py-3 px-4 text-sm focus:outline-blue-600"
+              id="email"
+              name="email"
+              onChange={onEmailChanged}
+              value={email}
+            />
+            <input
+              type="text"
+              placeholder="Phone No."
+              className="w-full border rounded py-3 px-4 text-sm focus:outline-blue-600"
+              id="phone"
+              name="phone"
+              onChange={onPhoneChanged}
+              value={phone}
+            />
+            <textarea
+              placeholder="Message"
+              rows="6"
+              className="w-full border rounded px-4 text-sm pt-3 focus:outline-blue-600"
+              id="message"
+              name="message"
+              onChange={onMessageChanged}
+              value={message}
+            ></textarea>
+            <button
+              type="submit"
+              className="text-white w-full bg-blue-600 hover:bg-blue-700 font-semibold rounded text-sm px-6 py-3"
+            >
+              Send Message
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
